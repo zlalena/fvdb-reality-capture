444ee8d53d4e779d90e04410ee947a8b56c9b26f
Mark Harris <mharris@nvidia.com>
2026-03-11 14:23:43 +1100

    Add __version__ attribute to fvdb_reality_capture package (#256)
    ## Summary
    
    - Expose `__version__` via `importlib.metadata.version()` in
    `fvdb_reality_capture/__init__.py`
    - Falls back to `"0.0.0.dev0"` for editable/uninstalled development
    installs
    - Exported in `__all__` for discoverability
    
    Fixes #254
    
    Signed-off-by: Mark Harris <mharris@nvidia.com>
    Signed-off-by: Zoe LaLena <zlalena@nvidia.com>

--- fvdb_reality_capture/__init__.py
+++ fvdb_reality_capture/__init__.py
@@ -2,10 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from importlib.metadata import PackageNotFoundError, version
+
 from . import dev, foundation_models, radiance_fields, sfm_scene, tools, transforms
 from .tools import download_example_data
 
+try:
+    __version__ = version("fvdb_reality_capture")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
+
 __all__ = [
+    "__version__",
     "dev",
     "foundation_models",
     "radiance_fields",
